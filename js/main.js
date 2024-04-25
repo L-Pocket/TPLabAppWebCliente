dfbc088310cfc1c44ee6e184b136fbc801ca1c93
Lautaro <croce.tato31@gmail.com>
2024-04-25 17:24:07 -0300

    cambios al carrito

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 const divContenedor = document.querySelector("div#container")
 const URL = "https://fakestoreapi.com/products"
-const carrito = []
 const arrayProductos = []
+const diccionarioCarrito = new Map()
+const diccionarioProductos = new Map()
 
 function cargarCarrito() {
     const carritoLS = JSON.parse(localStorage.getItem('carrito')) || [];
-
-    carritoLS.forEach(e => carrito.push(e))
+    
+    carritoLS.forEach(e => diccionarioCarrito.set(e.id+'', e.cantidad))
     actualizarProdsEnCarrito()
 }
 
@@ -17,8 +18,8 @@ function crearCardHTML(producto) {
                 <div class="producto-nombre"><p>${producto.title}</p></div>
                 <div class="producto-importe"><p>$ ${producto.price}</p></div>
                 <div class="btns-orden">
-                    <div ><button id="${producto.id}" class="btn-agregar">AGREGAR 🛒</button></div>
-                    <div ><button id="${producto.id}"class="btn-modal">Ver más</button></div>
+                    <div ><button id_producto=${producto.id} id="btn-agregar-${producto.id}" class="btn-agregar">AGREGAR 🛒</button></div>
+                    <div ><button id="${producto.id}" class="btn-modal">Ver más</button></div>
                 </div>                
             </div>`
 }
@@ -44,7 +45,9 @@ fetch(URL)
 .then((response) => response.json())
 .then((data) => {
     arrayProductos.push(...data)
+    data.forEach(producto => diccionarioProductos.set(producto.id+'', producto))
     cargarProductos()
+    cargarCarrito()
 })
 .catch((error) => {
     console.error('Error al obtener los productos:', error)
@@ -55,7 +58,7 @@ fetch(URL)
 function abrirAlerta(mensaje = '¡Haz agregado un producto al carrito!') {
     document.getElementById('alerta').style.display = 'block'
     document.getElementById('mensaje-alerta').innerText = mensaje
-    setTimeout(() => cerrarAlerta(), 2000)
+    setTimeout(() => cerrarAlerta(), 500)
 }
 
 function cerrarAlerta() {
@@ -70,16 +73,19 @@ const spanClose = document.getElementsByClassName("close")[0];
 const productoModal = document.querySelector('#contenedor-modal')
 
 function abrirModal(producto) {
-    modal.style.display = "block";
+    const idBtn = `btn-dagregar-${producto.id}`
+    modal.style.display = "block"
     //Estructura del Template literal
         productoModal.innerHTML = `<div>
                                 <div ><image class="producto-imagen" src="${producto.image}" alt="Imagen de producto"></image></div>
                                 <div class="producto-nombre"><p>${producto.title}</p></div>
                                 <div class="producto-importe"><p>$ ${producto.price}</p></div>
                                 <div class="btns-orden">
-                                    <div ><button id="${producto.id}" class="btn-agregar">AGREGAR 🛒</button></div>                                        
+                                    <div ><button id_producto=${producto.id} id=${idBtn} class="btn-agregar">AGREGAR 🛒</button></div>                                        
                                 </div>                
-                            </div>`
+                        </div>`
+
+    document.querySelector(`#${idBtn}`).addEventListener("click", () => agrergarProductoAlCarrito(producto.id))
 }
 
 function activarClickVerMas() {
@@ -87,7 +93,7 @@ function activarClickVerMas() {
     // Abrir Modal con click
     botonesVerMas.forEach((boton)=> {
         boton.addEventListener("click", ()=> { 
-            const producto = arrayProductos.find((producto) => producto.id == boton.id)
+            const producto = diccionarioProductos.get(boton.id)
             abrirModal(producto)      
         })
     })
@@ -108,17 +114,20 @@ const carritoContainer = document.getElementById("container-carrito")
 
 function actualizarProdsEnCarrito() {
     const spanCarrito = document.querySelector('span#spanContadorCarrito')
-    spanCarrito.textContent = carrito.length
+    spanCarrito.textContent = diccionarioCarrito.size > 0 ? Array.from(diccionarioCarrito.values()).reduce((a,b) => a+b) : 0
+    
+    document.querySelector('#carrito-total').innerHTML = "Total: $ "
+    document.querySelector('#carrito-total').innerHTML += diccionarioCarrito.size > 0 ? Array.from(diccionarioCarrito, ([idProducto, cantidad]) => ((diccionarioProductos.get(idProducto+'').price * cantidad) || 0).toFixed(2)).reduce((a,b) => parseFloat(a) + parseFloat(b)) : 0;
 
     const btnCarrito = document.querySelector("#btn-carrito")
-    const btnOcultarCarrito = document.querySelector("#ocultar-carrito")
-    if (carrito.length > 0) {
+
+    if (diccionarioCarrito.size > 0) {
         btnCarrito.style.display = "block"
         btnCarrito.disabled = false
     } else {
         btnCarrito.disabled = true
         // btnCarrito.style.display = "none"
-        btnOcultarCarrito.style.display = "none"
+        //btnOcultarCarrito.style.display = "none"
     }
 
     // if (carritoContainer.style.display === 'block') {
@@ -128,18 +137,13 @@ function actualizarProdsEnCarrito() {
 
     carritoContainer.innerHTML = ""
     
-    carrito.forEach((producto) => {
-        carritoContainer.innerHTML += `<div class="carrito-item">
-                                        <p>${producto.title}</p>                                        
-                                        <p class="carrito-item-precio">$ ${producto.price}</p>                                                                              
-                                      </div>`
-    })
+    Array.from(diccionarioCarrito.keys()).forEach(idProducto => carritoContainer.innerHTML += crearItemHTMLCarrito(diccionarioProductos.get(idProducto+'')))
 
     persistirCarrito()
 }
 
 function persistirCarrito() {
-    localStorage.setItem("carrito", JSON.stringify(carrito) || [])
+    localStorage.setItem("carrito", JSON.stringify(Array.from(diccionarioCarrito, ([idProducto, cantidad]) => ({ id: idProducto, cantidad }))))
 }
 
 const btnCarrito = document.querySelector("#btn-carrito")
@@ -151,13 +155,9 @@ function mostrarCarrito() {
     const carritoContainer = document.getElementById("container-carrito")
     carritoContainer.innerHTML = ""
     
-    if (carrito.length > 0) {
-        carrito.forEach((producto) => {
-            carritoContainer.innerHTML += `<div class="carrito-item">
-                                                <p>${producto.title}</p>                                        
-                                                <p class="carrito-item-precio">$ ${producto.price}</p>                                                                              
-                                            </div>`
-        })
+    if (diccionarioCarrito.size > 0) {
+        Array.from(diccionarioCarrito.keys()).forEach(idProducto => carritoContainer.innerHTML += crearItemHTMLCarrito(diccionarioProductos.get(idProducto+'')))
+    
         carritoContainer.style.display = "block"
         allCarritoContainer.style.display = "block"
         btnOcultarCarrito.style.display = "block"
@@ -173,6 +173,22 @@ function mostrarCarrito() {
     }
 }
 
+function crearItemHTMLCarrito(producto) {
+    return `<div class="carrito-item">
+        <p>${producto.title}</p>  
+        <div>                                       
+            <p class="carrito-item-precio">$ ${producto.price}</p>
+            <div class="controles-cantidad">
+                <button class="boton-restar" id="boton-restar-${producto.id}" ${diccionarioCarrito.get(producto.id+'') == 1 ? 'disabled' : ''} onclick="modificarCantidadItem(${producto.id}, -1)">-</button>
+                <span class="cantidad-productos">${diccionarioCarrito.get(producto.id+'')}</span>
+                <button class="boton-sumar" onclick="modificarCantidadItem(${producto.id}, 1)">+</button>
+                <button class="boton-eliminar" onclick="modificarCantidadItem(${producto.id}, -${diccionarioCarrito.get(producto.id+'')})">🗑️</button>
+            </div>
+        </div>
+    </div>`;
+}
+
+
 btnCarrito.addEventListener("click", mostrarCarrito)
 btnOcultarCarrito.addEventListener("click", function() {
     const carritoContainer = document.getElementById("container-carrito")
@@ -186,18 +202,32 @@ btnVaciarCarrito.addEventListener("click", limpiarCarrito)
 
 function activarClickEnBotones() {
     const botonesAgregar = document.querySelectorAll('button.btn-agregar')
-    
-    botonesAgregar.forEach((boton)=> {
-        boton.addEventListener("click", ()=> {
-            //alert('Hiciste click en el botón. Id: '+ boton.id)
-            const productoSeleccionado = arrayProductos.find((producto) => producto.id == boton.id)
-            //console.log(productoSeleccionado)
-            carrito.push(productoSeleccionado)
-            abrirAlerta(`¡Haz agregado correctamente tu ${productoSeleccionado.title} al carrito!`)
-            //console.table(carrito)
-            actualizarProdsEnCarrito()
-        })
-    })
+    botonesAgregar.forEach((boton) => boton.addEventListener("click", () => agrergarProductoAlCarrito(boton.attributes.id_producto.value)))
+}
+
+function agrergarProductoAlCarrito(idProducto){
+    //alert('Hiciste click en el botón. Id: '+ boton.id)
+    const productoSeleccionado = diccionarioProductos.get(idProducto+'')
+    //console.log(productoSeleccionado)
+    const cantidad = diccionarioCarrito.get(idProducto+'') || 0
+    diccionarioCarrito.set(idProducto+'', cantidad + 1)
+
+    abrirAlerta(`¡Haz agregado correctamente tu ${productoSeleccionado.title} al carrito!`)
+    //console.table(carrito)
+    actualizarProdsEnCarrito()
+}
+
+function modificarCantidadItem(idProducto, extra = 1){
+    const cantidad = diccionarioCarrito.get(idProducto+'') + extra
+
+    if(cantidad <= 0){
+        diccionarioCarrito.delete(idProducto+'')
+    }else{
+        diccionarioCarrito.set(idProducto+'', cantidad)
+    }
+
+    actualizarProdsEnCarrito()
+    console.log('jjj')
 }
 
 // ORDEN Y FILTRO BÚSQUEDA
@@ -245,12 +275,10 @@ const btnOrdenarCaros = document.getElementById("btn-ordenar-caros")
 btnOrdenarBaratos.addEventListener("click", () => ordenarProductosPorPrecio('ascendente'))
 btnOrdenarCaros.addEventListener("click", () => ordenarProductosPorPrecio('descendente'))
 
-cargarCarrito()
-
 // Limpiar carrito y realizar compra
 
 function limpiarCarrito() {
-    carrito.length = 0
+    diccionarioCarrito.clear()
     actualizarProdsEnCarrito()
     btnVaciarCarrito.style.display = 'none'
     carritoContainer.style.display = 'none'
@@ -261,7 +289,7 @@ function limpiarCarrito() {
 const btnRealizarCompra = document.getElementById('realizar-compra')
 
 function realizarCompra() {
-    carrito.length = 0
+    diccionarioCarrito.clear()
     actualizarProdsEnCarrito()
     btnVaciarCarrito.style.display = 'none'
     carritoContainer.style.display = 'none'
